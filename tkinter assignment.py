1b5c7665e90e5b2cad9d437785310d246219739d
Angelo Nicolo <AngeloCNicolo@gmail.com>
2023-05-17 16:55:59 -0700

    update page

--- tkinter assignment.py
+++ tkinter assignment.py
@@ -1,16 +1,16 @@
-#
+#tkinter module, brings classes and function for making graphical user interfaces.
 import tkinter as tk
 
-#
+#When button is clicked, it will print "Button clicked!"
 def button_click():
     print("Button clicked!")
 
 root = tk.Tk()
 root.title("Button Example")
 
-#
+#Function will be called when the button is clicked, while also adding buttons to the window to desplay  them.
 button = tk.Button(root,text="Click Me!", command=button_click)
 button.pack()
 
-#
+#keeps window open til user closes it, also listens and responds to the user.
 root.mainloop()
