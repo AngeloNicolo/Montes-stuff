9d241c24b036e6ab6186a50f7de7169f835ea691
Angelo Nicolo <AngeloCNicolo@gmail.com>
2023-05-18 11:33:30 -0700

    update

--- combobox.py
+++ combobox.py
@@ -7,5 +7,8 @@ from tkinter import ttk
 def on_select(event):
 
     selected_item = event.widget.get()
-   
+   print("Selected item;", selected_item)
 
+#
+rooot = tk.Tk()
+root.tile("Combobox Example")
