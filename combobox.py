eedebb41000aa6bf7bdfd15419a62511edd43ac3
Angelo Nicolo <AngeloCNicolo@gmail.com>
2023-05-18 11:39:18 -0700

    update

--- combobox.py
+++ combobox.py
@@ -1,14 +1,14 @@
-#
+# brings classes and function 
 import tkinter as tk 
 from tkinter import ttk
 
 
-#
+#Defining the event when selected.
 def on_select(event):
 
     selected_item = event.widget.get()
    print("Selected item;", selected_item)
 
-#
-rooot = tk.Tk()
+#Window title comes back with text.
+root = tk.Tk()
 root.tile("Combobox Example")
