f1fe4c3255ca2d390bb4f1fa90ca08a8bec6d82e
Angelo Nicolo <AngeloCNicolo@gmail.com>
2023-05-25 10:28:36 -0700

    update

--- combobox.py
+++ combobox.py
@@ -7,8 +7,19 @@ from tkinter import ttk
 def on_select(event):
 
     selected_item = event.widget.get()
-   print("Selected item;", selected_item)
+    print("Selected item:", selected_item)
 
 #Window title comes back with text.
 root = tk.Tk()
 root.tile("Combobox Example")
+
+# List of items in a Array.
+items = ["item 1", "item2", "item3", "item4", "item5"]
+
+# Connect the Array to the combobox.
+combo_box = ttk.Combobox(root, values=items)
+combo_box.bind("<<ComboboxSelected>>", on_select)
+
+combo_box.pack()
+
+root.mainloop()
